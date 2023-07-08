7a4a171c3e0e3276bd80e0a3278a922db044092b
Gabriel Caneira <138708118+GabrielCaneira@users.noreply.github.com>
2023-07-08 10:09:03 +0100

    PPT_0.2

--- PedraPapelTesoura.py
+++ PedraPapelTesoura.py
@@ -1,23 +1,41 @@
 import random
 
-opcoes = ['pedra', 'papel', 'tesoura']
+def ppt():
+    opcoes = ['pedra', 'papel', 'tesoura']
 
-print('|-------PEDRA PAPEL TESOURA-------|')
-print()
-print()
+    print()
+    print('|-------PEDRA PAPEL TESOURA-------|')
+    print()
 
-escolha = input('Pedra, papel ou tesoura? ')
-pc = random.choice(opcoes)
-print(f'O computador escolheu {pc}')
-print()
+    escolha = input('Pedra, papel ou tesoura? ')
+    pc = random.choice(opcoes)
+    print(f'O computador escolheu {pc}')
+    print()
 
-if escolha.lower() == pc:
-    print('EMPATE')
-elif escolha.lower() == 'pedra' and pc == 'tesoura':
-    print('GANHASTE! PARABÉNS!')
-elif escolha.lower() == 'papel' and pc == 'pedra':
-    print('GANHASTE! PARABÉNS!')
-elif escolha.lower() == 'tesoura' and pc == 'papel':
-    print('GANHASTE! PARABÉNS!')
-else:
-    print('QUE PENA. PERDESTE.')
+    if escolha.lower() == pc:
+        print('EMPATE')
+    elif escolha.lower() == 'pedra' and pc == 'tesoura':
+        print('GANHASTE! PARABÉNS!')
+    elif escolha.lower() == 'papel' and pc == 'pedra':
+        print('GANHASTE! PARABÉNS!')
+    elif escolha.lower() == 'tesoura' and pc == 'papel':
+        print('GANHASTE! PARABÉNS!')
+    elif escolha.lower() not in opcoes:
+        print(f'Opcção {escolha.upper()} inválida. Escolhe entre Pedra, Papel ou Tesoura')
+    else:
+        print('QUE PENA. PERDESTE.')
+    novo_jogo()
+
+def novo_jogo():
+    jogar = input('JOGAR DE NOVO? (S/N): ').lower()
+    if jogar == 's':
+        ppt()
+    elif jogar == 'n':
+        print()
+        print(' |-----------FIM DO JOGO----------|')
+    else:
+        print('Opcção inválida.')
+        print('JOGAR DE NOVO? (S/N): ')
+        novo_jogo()
+
+ppt()

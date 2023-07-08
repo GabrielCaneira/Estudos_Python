172946f5687f82bdca5cd9882cc1a1c99c96f678
Gabriel Caneira <138708118+GabrielCaneira@users.noreply.github.com>
2023-07-08 13:17:16 +0100

    PPT_1.0 com sistema de pontuação do jogador vs computador

--- PedraPapelTesoura.py
+++ PedraPapelTesoura.py
@@ -1,29 +1,32 @@
 import random
 
+print()
+print('ATENÇAO:')
+print('A escolha do computador é completamente aleatoria')
+print('A tua jogada não afecta a sua escolha')
 def ppt():
     opcoes = ['pedra', 'papel', 'tesoura']
 
     print()
     print('|-------PEDRA PAPEL TESOURA-------|')
+    ver_pontos()
     print()
 
     escolha = input('Pedra, papel ou tesoura? ')
     pc = random.choice(opcoes)
-    print(f'O computador escolheu {pc}')
+    print(f'O computador escolheu: {pc.upper()}')
     print()
 
     if escolha.lower() == pc:
         print('EMPATE')
-    elif escolha.lower() == 'pedra' and pc == 'tesoura':
+    elif escolha.lower() == 'pedra' and pc == 'tesoura' or escolha.lower() == 'papel' and pc == 'pedra' or escolha.lower() == 'tesoura' and pc == 'papel':
         print('GANHASTE! PARABÉNS!')
-    elif escolha.lower() == 'papel' and pc == 'pedra':
-        print('GANHASTE! PARABÉNS!')
-    elif escolha.lower() == 'tesoura' and pc == 'papel':
-        print('GANHASTE! PARABÉNS!')
+        mais_pontos('jogador')
     elif escolha.lower() not in opcoes:
         print(f'Opcção {escolha.upper()} inválida. Escolhe entre Pedra, Papel ou Tesoura')
     else:
         print('QUE PENA. PERDESTE.')
+        mais_pontos('computador')
     novo_jogo()
 
 def novo_jogo():
@@ -33,8 +36,25 @@ def novo_jogo():
     elif jogar == 'n':
         print()
         print('|-----------FIM DO JOGO----------|')
+        ver_pontos()
     else:
         print('Opcção inválida.')
         novo_jogo()
+
+def ver_pontos():
+    print(f'   |JOGADOR: {pontos_jogador}  | COMPUTADOR: {pontos_pc}|')
+
+def mais_pontos(pontuacao):
+    if pontuacao == 'jogador':
+        global pontos_jogador
+        pontos_jogador += 1
+    else:
+        pontuacao == 'computador'
+        global pontos_pc
+        pontos_pc +=1
+
+
+pontos_jogador = 0
+pontos_pc = 0
 
 ppt()
